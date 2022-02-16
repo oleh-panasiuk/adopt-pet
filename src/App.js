004162b42765d5e2af5d1bcdd8661ae88567c35a
Oleh Panasiuk <panasiukolehs@gmail.com>
2022-02-16 13:48:03 +0200

    composition a bit and add hooks to project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from "react";
+import { StrictMode } from "react";
 import reactDom from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "DorianSas",
-      animal: "Cat",
-      color: "Red",
-    }),
-    React.createElement(Pet, { name: "Dorian", animal: "Cat", color: "Red" }),
-    React.createElement(Pet, { name: "Dorian", animal: "Cat", color: "Red" }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-reactDom.render(React.createElement(App), document.getElementById("root"));
+reactDom.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
