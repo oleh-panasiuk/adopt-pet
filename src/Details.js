1c354e6c6d8c1da436ba7235d9228c3647a9727c
Oleh Panasiuk <panasiukolehs@gmail.com>
2022-02-19 04:57:37 +0200

    add carousel

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,53 @@
+import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
 
-const Details = () => {
-  const { id } = useParams();
-  return <h2>{id}</h2>;
-};
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let params = useParams();
+    return <Component {...props} router={{ params }} />;
+  }
 
-export default Details;
+  return ComponentWithRouterProp;
+}
+
+class Details extends Component {
+  constructor() {
+    super();
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    console.log(this.props);
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.router.params.id}`
+    );
+    const json = await res.json();
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+  }
+
+  render() {
+    console.log(this.state);
+
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+
+    return (
+      <div className="details">
+        <Carousel images={images} />;
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
